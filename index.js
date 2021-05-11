10db4f27e2e69521c48eefb0d58558c44c0d2d82
Marazorq <shuratov2001@mail.ru>
2021-05-12 00:31:45 +0300

    added some dependendses and created test end point

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 require('dotenv').config()
 const sequelize = require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
+// чтобы можно было отправлять запросы с браузера
+app.use(cors())
+// чтобы парсить json формат
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Is work!'})
+})
 
 const start_app = async () => {
     try{
