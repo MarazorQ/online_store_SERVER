c53732bcbc19814ad998156d6bafd2be5fbd3e43
Marazorq <shuratov2001@mail.ru>
2021-05-11 23:37:33 +0300

    created connection to DB

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 require('dotenv').config()
-
+const sequelize = require('./db')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT}`))
+const start_app = async () => {
+    try{
+        await sequelize.authenticate()// connect to bd
+        await sequelize.sync()// сверяет данные со схемой данных
+        app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT}`))
+    }catch (e){
+        console.log(`ERROR ${e}`)
+    }
+}
+
+start_app()
