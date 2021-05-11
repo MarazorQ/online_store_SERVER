6185e76b4d6c9ed66d24b1f4c1b289f35ac0c7ee
Marazorq <shuratov2001@mail.ru>
2021-05-12 01:17:04 +0300

    added generic request handler

---
+++ errors/apiErrors.js
@@ -0,0 +1,18 @@
+class ApiErrors extends Error{
+    constructor(status, message){
+        super();
+        this.status = status
+        this.message = message
+    }
+
+    static badReq(message){
+        return new ApiErrors(404, message)
+    }
+    static iternal(message){
+        return new ApiErrors(500, message)
+    }
+    // доступа нет
+    static forbidden(message){
+        return new ApiErrors(403, message)
+    }
+}
