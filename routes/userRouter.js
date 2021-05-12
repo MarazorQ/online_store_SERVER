26498cc6ed7adc71a199be5fdc9aa12ea854a1e5
Marazorq <shuratov2001@mail.ru>
2021-05-12 03:52:41 +0300

    added new middlaware for auth

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddlware = require('../middlewares/authMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.auth)
+router.get('/auth', authMiddlware, userController.auth)
 
 module.exports = router
